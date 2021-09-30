ae66f3645e9b5a9e7102673615f5c00d2300b657
Leonardo Adamini <leonardoadamini@gmail.com>
2021-09-30 19:33:51 -0300

    Starting Final Proyect

--- src/Profile.js
+++ src/Profile.js
@@ -1,8 +1,25 @@
 import React from "react";
 import { useAuth0 } from "@auth0/auth0-react";
+import request from "request"
+
+
 
 export const Profile = () => {
+    
   const { user, isLoading } = useAuth0();
+
+
+
+  var options = { method: 'POST',
+    url: 'https://dev-k4p8d93b.us.auth0.com/oauth/token',
+    headers: { 'content-type': 'application/json' },
+    body: '{"client_id":"x5iP6nUyoqJEntgtcncoPFFqEUPXrH02","client_secret":"w99yX4q4fx2aVxpHRRAzayZuMFVRd0z6rMy_dp6wcy_TBYGZmEbqQ2GooHscrepp","audience":"https://dev-k4p8d93b.us.auth0.com/api/v2/","grant_type":"client_credentials"}' };
+  
+  request(options, function (error, response, body) {
+    if (error) throw new Error(error);
+  
+    console.log(body);
+  });
 
   if (isLoading) {
       return <div>Loading...</div>
